956dd98367ddbab83e661a663d7c18e49d8cfe0b
faisalmirji123 <faisalmirji@gmail.com>
2021-11-16 14:49:33 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './style.css';
-import Header from './Header.jsx';
+
 export default function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+  return <div className="App">
+    
+  </div>;
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
+// import React from 'react';
 
-const Header = () => {
-  return <nav>
-    <ul className = "right">
-      <li><a href="">Menu</a></li>
-      </ul>
-      </nav>
-     <div className= "menu-btn">
-       <div className = "menu-btn_burger"></div>
-       </div>
+// const Header = () => {
+//   return <nav>
+//     <ul className = "right">
+//       <li><a href="">Menu</a></li>
+//       </ul>
+//       </nav>
+//      <div className= "menu-btn">
+//        <div className = "menu-btn_burger"></div>
+//        </div>
 
-
-      
-
-};
-export default Header;
+// };
+// export default Header;
